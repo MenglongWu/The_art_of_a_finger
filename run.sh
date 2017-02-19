1d293431516a6a0d23e14852d4d59ca6c91a5ed2
Menglong Woo <MenglongWoo@aliyun.com>
2017-02-19 23:01:16 +0800

    Edit(run.sh)

--- run.sh
+++ run.sh
@@ -5,16 +5,54 @@ export PATH=${PATH}:${RUN_DIR}/bin
 export H1=0
 export H2=0
 export H3=0
-. comon_style.sh
+export H4=0
+. common_style.sh
 . test.sh
 
 # function 
 # Title
 # com11
+# OpenResource python su
+
+# O=`getopt   a:c:P:: "$@"`
+# # eval set -- "$O"
+# echo [$O]
+# while  [ -n "$1" ]
+# do
+#     # echo "\$1 is $1"
+#     case $1 in
+#         -a)
+#             echo aaaa
+#             echo $@
+#             ;;
+#         -c)
+#             echo ccc
+#             ;;
+#         -P)
+#             echo PP
+#             echo $@
+#             ;;
+# 	esac
+# 	shift
+# done
 
 
-Title
 
+Title "基本栈"
+
+# ReportParam -a "pa1 pa2 p3" -b b3 -c c4 -a "go"
+# ReportTrace default.elf
+# ProcessStack $(pidof python)
+# ReportOpenResource -a "python su" -b f 
+# ReportOpenResource -a "pa1 pa2 p3" -b b3 -c c4 -a "go"
+# _getopt_openresource -p "python bash" -t  -m -s 32 -s 2
+# _getopt_openresource -p "python bash gdm-session-worker" -t  -m 
+_getopt_openresource -p "default.elf" 
+ReportNetState
+# ReportOpenResource -a 'pa1 pa2 p3' -b b3 -c c4 -a "go"
+# ReportTrace python
+
+exit
 # Memory
 Top
 ReportDiskSpace
@@ -28,6 +66,7 @@ Interrupt
 # OpenResource default.elf
 # OpenResource python
 OpenResource python su
+
 
 # NetServer
 #while [ 1 ]
