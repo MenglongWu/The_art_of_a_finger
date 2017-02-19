05da1f45d3f881609ccac6e9ab9ab485a429f73b
Menglong Woo <MenglongWoo@aliyun.com>
2017-02-19 23:03:43 +0800

    Add(misc):use 'getopts' parse

--- bin/test.sh
+++ bin/test.sh
@@ -107,12 +107,16 @@ function _2net_arp() {
 	# sort 以 ':' 做分割排序mac
 	# uniq 找到重复项（uniq必须与sort联合使用）
 	conflict=$(cat /proc/net/arp | awk -F ' ' '{if (NR>1) print $4}' | sort -t ':' | uniq -d)
-	SubjectTitle_Warrning "ARP冲突"
-	Font_Color_Start red
+	
 	if [ "${conflict}" != "" ]
 	then
+		SubjectTitle_Warrning "ARP冲突"
 		Form_Start
-	    echo ${conflict}
+		for var in ${conflict[@]}
+		do
+			echo -e ${var}
+		done
+	    
 	    Form_End
 	fi  
 	Font_Color_End
@@ -146,8 +150,27 @@ function OpenFiles() {
 	((H4++))
 	SubjectTitle_H3 "${H1}.${H2}.${H3}.${H4} ${2}(${1}) 打开文件数"
 	Form_Start
-	ls /proc/${1}/fd -l
+	total=$(ls /proc/${1}/fd -l | wc -l)
+	((total--))
+	echo "打开文件总数" $total
+	ls /proc/${1}/fd -l | head -n 10
+	if [ ${total} -gt 10 ]
+	then
+		echo "..........余下省略.........."
+	fi
 	Form_End
+
+
+	
+	
+	if [ ${total} -gt 100 ]
+	then
+		SubjectTitle_Warrning "是否存在未释放文件资源"
+		Form_Start
+	    echo "打开文件总数" $total
+	    Form_End
+	fi  
+	
 }
 
 
@@ -155,7 +178,7 @@ function ProcessTree() {
 	((H4++))
 	SubjectTitle_H3 "${H1}.${H2}.${H3}.${H4} ${2}(${1}) 进程树"
 	Form_Start
-	pstree -p ${1}
+	pstree -pn ${1}
 	Form_End
 }
 
@@ -179,10 +202,54 @@ function ProcessStack() {
 	((H4++))
 	SubjectTitle_H3 "${H1}.${H2}.${H3}.${H4} ${2}(${1}) 栈"
 	Form_Start
-	pstack ${1}
-	pstack ${1}
-	pstack ${1}
+	echo arg3 ${3}
+	for (( i = 0; i < ${3}; i++ ))
+	do
+		pstack ${1}
+	done
 	Form_End
+}
+export -f ProcessStack
+
+function ProcessStatus() {
+	((H4++))
+	SubjectTitle_H3 "${H1}.${H2}.${H3}.${H4} ${2}(${1}) status"
+	Form_Start
+	cat /proc/${1}/status
+	Form_End
+}
+
+function ProcessStat() {
+	((H4++))
+	SubjectTitle_H3 "${H1}.${H2}.${H3}.${H4} ${2}(${1}) stat"
+	item=($(cat /proc/${1}/stat))
+	# item=(1 2)
+	# Form_Start
+	# for var in ${item[@]}
+	# do
+	
+
+	var_flt="min_flt ${item[9]}  \n \
+			cmin_flt   ${item[10]}  \n \
+			maj_flt    ${item[11]}  \n \
+			cmaj_flt   ${item[12]}  \n"
+
+	var_time="utime ${item[13]}  \n \
+			stime   ${item[14]}  \n \
+			cutime    ${item[15]}  \n \
+			cstime   ${item[16]}  \n"
+				# \"- maj_flt\" ${item[11]}  \"\n\"\
+				# \"- cmaj_flt\" ${item[12]}  \"\n\""
+	# var_flt="\"a b\" \"cs\""
+	# echo -e " - 任务状态\t" ${item[2]} "\n"\
+	# 			"- ppid" ${item[3]}  "\n"\
+	# 			${var_flt} \
+	# 			${var_time}
+	echo -e ${var_flt}
+	echo -e ${var_time}
+	# done
+	
+	# Form_End
 }
 
 function _2param()
@@ -200,15 +267,18 @@ function _2param()
 		echo 同名PID ${pid}
 		Form_End
 
-
-	 	for var in ${pid[@]};do
+		
+	 	for var in ${pid[@]}
+	 	do
 	 		H4=0
-	 		((H3++))
+	 	 	((H3++))
 			ProcessTree $var $1
 			ProcessMem  $var $1
-			ProcessStack  $var $1
-			# OpenFiles $var
+			ProcessStack  $var $1 2
+			OpenFiles $var
 		done
+		# let "H3++"
+
 	 	
       	shift
   	done
@@ -263,6 +333,202 @@ function ReportIO() {
 export -f ReportIO
 
 
+function _2trace_detail() {
+	((H2++))
+	SubjectTitle_H2 "${H1}.${H2} 跟踪详细"
+	strace -o ${1}.trace -Ttt -p $(pidof ${1}) &
+	sleep 5
+	kill $(pidof strace)
+	Form_Start
+	cat ${1}.trace
+	Form_End
+}
 
+function _2trace_count() {
+	((H2++))
+	SubjectTitle_H2 "${H1}.${H2} 跟踪计数"
+	strace -o ${1}.trace.c -Ttt -p $(pidof ${1}) -c &
+	sleep 5
+	kill $(pidof strace)
+	Form_Start
+	cat ${1}.trace.c
+	Form_End
+}
+
+function ReportTrace() {
+	H2=0;
+	((H1++))
+	SubjectTitle_H1 "${H1} ${1} IO吞吐"
+	# _2trace_detail $@
+	# _2trace_count $@
+}
+export -f ReportTrace
+
+function _getopt_openresource() {
+	# echo "param number" $#
+
+
+
+	# 分析参数
+	while getopts "ctmp:s:" argv
+	do
+		case $argv in
+			p)
+				rep_process=1
+				rep_process_argv=$OPTARG
+				# echo [$OPTARG]
+				;;
+			c)
+				# echo [$OPTARG]
+				;;
+			t)
+				rep_tree=1
+				# echo [$OPTARG]
+				;;
+			m)
+				rep_mem=1
+				# echo [$OPTARG]
+				;;
+			s)
+				rep_stack=1
+				rep_stack_argv=$OPTARG
+				# echo [$OPTARG]
+				;;
+			# TODO 添加新选项
+		esac
+	done
+
+	# echo process $rep_process argv $rep_process_argv
+	# echo stack $rep_stack argv $rep_stack_argv
+
+	# 
+	if [ ${rep_process} -eq 0 ]
+	then
+		return
+	fi
+	a=Title
+	# 逐个进程名处理
+	
+	((H1++))
+	SubjectTitle_H1 "${H1} 资源"
+	for var in ${rep_process_argv[@]};do
+		H3=0
+		((H2++))
+
+		pids=$(pidof $var)
+		SubjectTitle_H2 "${H1}.${H2} ${var}"
+		Form_Start
+		echo 同名PID ${pids}
+		Form_End
+
+
+
+
+		for onepid in ${pids[@]};do
+			H4=0
+			((H3++))
+
+
+			if [ ${rep_tree} ]
+			then
+				ProcessTree ${onepid} ${var}
+			fi
+
+
+			if [ ${rep_mem} ]
+			then
+				ProcessMem ${onepid} ${var}
+			fi
+
+
+			if [ ${rep_stack} ]
+			then
+				ProcessStack ${onepid} ${var} ${rep_stack_argv}
+			fi
+
+			OpenFiles ${onepid} ${var}
+			ProcessStatus ${onepid} ${var}
+			ProcessStat ${onepid} ${var}
+			# TODO 有什么新的功能在这添加
+		done
+	done
+}
+export -f _getopt_openresource
+
+function ReportOpenResource() {
+	H2=0
+	((H1++))
+	
+	SubjectTitle_H1 "${H1} 资源"
+	echo "param nu" $#
+	k=(-a 'pa1 pa2 p3' -b b3 -c c4 -a "go")
+
+	g='getopts "a:b:c:" -- "$@"'
+	echo "g="  $g
+	for i in $@; do
+	    echo ---$i
+	done
+
+	_getopt_openresource ${k[@]}
+}
+export -f ReportOpenResource
+
+
+function ReportParam() {
+	H2=0;
+	((H1++))
+	SubjectTitle_H1 "${H1} ${1} param"
+	
+	echo '$*' $*
+	echo '$@' $@
+	echo '$#' $#
+	echo '$_' $_
+	echo '$$' $$
+	echo '$!' $!
+	echo '$?' $?
+	echo '$1' $1
+	echo '$1' $1
+	echo '$2' $2
+	echo '$3' $3
+	echo '$4' $4
+	echo '$5' $5
+
+	k=(${2})
+	# k=(pa1 pa2 p3)
+	# echo ${k}
+	# echo 'k1' ${k[2]} '#{k}'${#k[@]}
+	echo ------
+	echo k[0]
+	echo k[1]
+	echo k[2]
+	echo k[3]
+
+
+	for var in ${k[@]};do
+		echo $var
+	done
+
+	echo "do getopts"
+	while getopts "a:b:c" arg
+	do
+		case $arg in
+			a)
+				echo [$OPTIND] [$OPTARG]
+				;;
+			b)
+				echo [$OPTIND] [$OPTARG]
+				;;
+		esac
+	done
+	echo ---${H3}
+	# $OPTIND
+
+
+
+}
+export -f ReportParam
 # 内核栈
 # watch -n 1 cat /proc/$(pidof default.elf)/stack
+
+# strace -o default.elf -Ttt -p $(pidof default.elf) 
+# pstack $(pidof default.elf)  
