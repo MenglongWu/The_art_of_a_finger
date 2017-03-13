fb9a67da2043e1ba7d7232ded7fbd8579ea96b85
Menglong Woo <MenglongWoo@aliyun.com>
2017-03-13 20:06:18 +0800

    Refactor(network):comments ! instead of #

--- network/cisco/switch3.sh
+++ network/cisco/switch3.sh
@@ -1,40 +1,91 @@
 #!/bin/bash
 . common.sh
-function create_vlan() {
-	echo "vlan database"
-	echo "vlan ${1}"
-	echo "exit"
-}
 
-
-# 验证过
-# $1 interface
-# $2 vlan id
-function interface_to_vlan() {
+# $1 vlan 号
+# $2 vlan 别名
+function create_vlan_by_name() {
+	code_comments $FUNCNAME $@
 	param_less $FUNCNAME $# 2
 
-	create_vlan ${1}
+	echo "vlan database"
+	echo "vlan 				${1}"
+	echo "exit"
 
-	echo "conf t"
-	echo "int ${2}"
-	echo "no sh"
-	echo "switchport mode access"
-	echo "sw acc vlan ${1}"
-	echo -e "exit\nexit\n"
+	
+	if [ "$#" -gt 1 ]
+	then
+		echo "conf t"
+		echo "vlan 				${1}"
+		echo "name 				${2}"
+		echo -e "exit\nexit"
+	fi
+
+
+
 }
 
+function create_mutil_vlan() {
+	param_less $FUNCNAME $# 1
+
+	echo "vlan database"
+	
+	declare -a vlan_s=($@)
+	for vlan in "${vlan_s[@]}"
+	do
+		echo "vlan 				${vlan}"
+	done
+	
+	echo "exit"
+
+
+}
+
+
+
+############################################################
+# 验证过
+# $1 vlan id
+# $2 interface
+function interface_to_vlan() {
+	code_comments $FUNCNAME $@
+	param_less $FUNCNAME $# 2
+	vlan_num=${1}
+	shift
+	declare -a intface_s=($@)
+
+	create_mutil_vlan ${vlan_num}
+
+	echo "conf t"
+	for int in "${intface_s[@]}"
+	do
+		echo "interface 			${int}"
+		echo "no shutdown"
+		echo "switchport mode access"
+		echo "switchport access vlan ${vlan_num}"
+		echo "exit"
+	done
+	
+	echo "exit"
+}
 
 # 验证过
 # $1 interface
 function interface_to_trunk() {
+	code_comments $FUNCNAME $@
 	param_less $FUNCNAME $# 1
 
+	declare -a intface_s=($@)
+	
 	echo "conf t"
-	echo "int ${1}"
-	echo "no sh"
-	echo "switchport trunk encapsulation dot1q"
-	echo "switchport mode trunk"
-	echo -e "exit\nexit\n"
+	for int in "${intface_s[@]}"
+	do
+		echo "int 				${int}"
+		echo "no shutdown"
+		echo "switchport trunk encapsulation dot1q"
+		echo "switchport mode trunk"
+		echo "exit"
+	done
+	echo -e "exit\n"
 }
 
 # $1 interface
@@ -74,6 +125,7 @@ function sw3_int_dot1q() {
 # $2 IP
 # $3 mask
 function vlan_ip() {
+	code_comments $FUNCNAME $@
 	param_less $FUNCNAME $# 3
 
 
@@ -89,7 +141,7 @@ function vlan_ip() {
 # $2 vlan
 function add_vlan_to_trunk() {
 	param_less $FUNCNAME $# 2
-	create_vlan ${2}
+	create_mutil_vlan ${2}
 
 
 	echo "conf t"
@@ -101,6 +153,7 @@ function add_vlan_to_trunk() {
 
 # 
 function ip_routing() {
+	code_comments $FUNCNAME $@
 	echo "conf t"
 	echo "ip routing"
 	echo -e "exit\n"
